b186a2796644c774b39dd3e71c71bfeb94525927
miasarasmita <91304286+miasarasmita@users.noreply.github.com>
2021-11-26 13:54:23 +0700

    Initial commit

---
+++ system/resources/views/admin/produk/show.blade.php
@@ -0,0 +1,37 @@
+@extends('admin.template.base')
+@section('content')
+
+<!-- start coding -->
+
+   
+   <div class="container">
+      <div class="card mt-3 pt-3">
+         <div class="card-body">
+            <div class="container-fluid">
+                  <div class="row ml-1 mr-2 mb-3">
+                     <h4>Detail Data Produk</h4>
+                  </div>
+
+      <div class="container">
+         <div class="card-body">
+            <h3>{{$produk->nama}}</h3>
+            <hr>
+            <p>{{$produk->harga}} |
+               Stok : {{$produk->stok}} |
+               Seller : {{$produk->seller->nama}} |
+               Tanggal Produk : {{$produk->created_at->format("d M Y")}}
+            </p>
+            {!! nl2br ($produk->deskripsi) !!}
+            </div>   
+         </div>
+         
+      </div>
+
+            </div>
+         </div>
+      </div>
+   </div>
+
+<!-- end coding -->
+
+@endsection
